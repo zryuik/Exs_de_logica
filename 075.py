76f55eed5ad6e99e5dc57183bbc5787e8263630b
zryuik <yuristronda123@hotmail.com>
2025-08-28 16:26:13 -0300

    .

--- 075.py
+++ 075.py
@@ -5,4 +5,12 @@ print(f"Você digitou os valores {num}")
 
 print(f"O valor 9 apareceu {num.count(9)} vezes ")
 
-print(f"o valor apareceu na {num.index(3))}ª")
+if 3 in num:
+    print(f"O valor 3 apareceu na {num.index(3)+1}ª")
+else:
+    print("O valor 3 não foi digitado")
+
+print(f"Os valores pares digitados foram ", end+)
+for numeros in num:
+    if numeros % 2 == 0:
+        print(numeros, end=' ')
