f2e6ce0f2df7a21f77cc03f2e549c28eb1abc668
zryuik <yuristronda123@hotmail.com>
2025-09-18 12:17:32 -0300

    ex feito

---
+++ 084.py
@@ -0,0 +1,23 @@
+temp = []
+princ = []
+mai = men = 0
+while True:
+    temp.append(str(input("Nome: ")))
+    temp.append(float(input("Peso: ")))
+    if len(princ) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp = str(input("Quer continuar? [S/N] "))
+    if resp in "nN":
+        break
+print("-" * 30)
+print(f"Os dados foram {princ}")
+print(f"Ao todo, você cadastrou {len(princ)} pessoas. ")
+print(f"O maior peso foi de {mai}Kg")
+print(f"O menor peso foi de {men}Kg.")
---
+++ 085.py
@@ -0,0 +1,2 @@
+for i in range(7):
+    valores = int(input(f"Digite o {i+1}º valor: "))
