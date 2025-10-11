6250a84b70dcae5d62462a9b2977966679809be4
zryuik <yuristronda123@hotmail.com>
2025-10-11 11:13:52 -0300

    Soluçao definitiva para o exercicio 096 do curso em video python mundo 3

--- 096.py
+++ 096.py
@@ -1,10 +1,11 @@
-def area(largura, comprimento):
-    a = largura * comprimento
-    print(f"A área de um terreno {largura}x{comprimento} é de {a}m²")
+''' Exercício Python 096: Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno '''
 
+def area(largura,comprimento):
+    area = (largura * comprimento)
+    print(f"A área de um terreno {largura} x {comprimento} é de {area:.2f} m².")
 
-print("Controle De Terrenos")
-print("-" * 20)
-l = float(input("LARGURA (m): "))
-c = float(input("COMPRIMENTO (m): "))
-area(l, c)
+largura = float(input("Digite a largura (m): "))
+comprimento = float(input("Digite o comprimento (m): "))
+
+area(largura,comprimento)
+
