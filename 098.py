b6867552bb578c5742c311f0bc93924a111a3a99
zryuik <yuristronda123@hotmail.com>
2025-10-11 11:44:11 -0300

    Soluçao definitiva para o exercicio 098 do curso em video python mundo 3

--- 098.py
+++ 098.py
@@ -1,39 +1,37 @@
+''' Exercício Python 098: Faça um programa que tenha uma função chamada contador(), que receba três parâmetros: início, fim e passo. Seu programa tem que realizar três contagens através da função criada:                                                                                                                                                                            a) de 1 até 10, de 1 em 1                                                                                                                                              b) de 10 até 0, de 2 em 2                                                                                                                                            c) uma contagem personalizada '''
+
+
+
 from time import sleep
-def contador(a, b, c):
-    if c < 0:
-        c *= -1
-    if c == 0:
-        c = 1
-    print('-' *30)
-    print(f"Contagem de {a} ate {b} de {c} em {c}")
-    print('-' * 30)
-    sleep(2.5)
-
-    if a < b:
-        cont = a
-        while cont <= b:
-            print(f"{cont} ", end="")
+def contador(inicio, fim, passo):
+    print(f"Contando de {inicio} ate {fim} de {passo} em {passo}.")
+    print("-" * 40)
+    sleep(1)
+    if passo == 0:
+        passo = 1
+    if inicio < fim:
+        for c in range(inicio,fim + 1, passo):
+            print(c, end=" ")
             sleep(0.5)
-            cont += c
-        print("FIM")
+            
     else:
-        cont = a
-        while cont >= b:
-            print(f"{cont} ", end=" ")
+        for c in range(inicio, fim - 1, -abs(passo)):
+            print(c, end=" ")
             sleep(0.5)
-            cont -= c
-        print('FIM')
+    print("\nFIM")
+    print("-" * 40)
 
-#programa principal
+
 contador(1, 10, 1)
-contador(10,0,2)
-print("Agora é sua vez de personalizar a contagem! ")
-ini = int(input("Ínicio: "))
-fim = int(input("Fim: "))
-pas = int(input("Passo: "))
-contador(ini, fim, pas)
+contador(10, 0, 2)
 
+print("Agora é sua vez!")
 
+i = int(input("Início: "))
+f = int(input("Fim: "))
+p = int(input("Passo: "))
+
+contador(i, f, p)
 
 
 
