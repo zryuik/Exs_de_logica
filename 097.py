0189a30b7079720af059c47b5cbc95cd23640a13
zryuik <yuristronda123@hotmail.com>
2025-10-11 11:20:00 -0300

    solução definitiva para o ex 097 do curso em video python mundo 3

--- 097.py
+++ 097.py
@@ -1,10 +1,10 @@
+'''Exercício Python 097: Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.                                  Ex:                                                                                                                                                                        escreva(‘Olá, Mundo!’) Saída:                                                                                                                          ~~~~~~~~~                                                                                                                                                            Olá, Mundo!                                                                                                                                                          ~~~~~~~~~                     '''
+
 def escreva(msg):
-    tam = len(msg) + 4
-    print("~" * tam)
-    print(f"  {msg}")
-    print("~" * tam)
+    tamanho = len(msg) + 4
+    print("~" * tamanho)
+    print(f"{msg:^{tamanho}}")
+    print("~" * tamanho)
 
-
-#programa principal
-escreva("z")
-
+escreva("Yure")
+escreva("Exercicio Feito")
