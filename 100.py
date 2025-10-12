36812fbd7565af0304b8feefc817beb727ba7149
zryuik <yuristronda123@hotmail.com>
2025-10-12 12:00:56 -0300

    Soluçao definitiva para o exercicio 100 do curso em video python mundo 3

--- 100.py
+++ 100.py
@@ -1,19 +1,24 @@
 ''' Exercício Python 100: Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar(). A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior. '''
+
+from time import sleep
 from random import randint
-numeros = []
 
-def sorteia():
-    num = randint(0, 999)
-    numeros.append(num)
+def sorteia(numeros):
+    print("Sorteando 5 valores da lista: ", end="")
+    for contador in range(0, 5):
+        numero = randint(1, 10)
+        numeros.append(numero)
+        print(f"{numero} ", end="", flush=True)
+        sleep(0.3)
+    print(" CONCLUIDO!")
 
-for _ in range(5):
-    sorteia()   
-print(numeros)
-
-
-def somapar():
+def somapar(numeros):
     soma = 0
     for num in numeros:
         if num % 2  == 0:
             soma += num
-    pri
+    print(f"Foram gerados {len(numeros)} números, e a soma dos números pares é {soma}")
+
+numeros = []
+sorteia(numeros)
+somapar(numeros)
