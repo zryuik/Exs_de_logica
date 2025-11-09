6a06a18998cede57146583a2536fedc14c81e42c
zryuik <yuristronda123@hotmail.com>
2025-11-09 16:04:29 -0300

    melhorei o codigo diminuindo para duas linhas

--- 007.py
+++ 007.py
@@ -1,4 +1,5 @@
 n1 = float(input('Digite uma nota: '))
 n2 = float(input('Digite outra nota: '))
-m = (n1 + n2) / 2 
-print(f'A Média desse aluno é: {m:.1f}')
+
+
+print(f'A Média desse aluno é: {(n1*n2) / 2 :.1f}')
