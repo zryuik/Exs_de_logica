aca12b289265e0cf6efd1a5470781fe148b61af0
zryuik <yuristronda123@hotmail.com>
2025-12-16 22:00:09 -0300

    ....

--- 002.py
+++ 002.py
@@ -1,4 +1,5 @@
 #Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
 
 nome = input('Digite seu nome: ')
-print(f'É um prazer te conhecer, {nome}!')
+print(f'É um prazer te conhecer, {nome}')
+print("É um prazer te conhecer, ", nome)
